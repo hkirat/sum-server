9b3bb519685cebcbd5dd9b894f18951d12c2a0c4
Harkirat Singh <harkirat96@gmail.com>
2023-12-23 17:41:11 +0530

    Added sum

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/sum", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const sum = a + b;
+  res.send(sum.toString());
+});
+
+app.listen(8080)

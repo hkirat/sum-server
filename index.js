24b8cc84fb94a75c3da65da5c7dae8a31eed08a6
Harkirat Singh <harkirat96@gmail.com>
2023-12-24 17:06:54 +0530

    added todos

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
+
+const todos = [{
+  id: 1,
+  title: "Todo 1",
+  descripton: "This is todo 1",
+  completed: false,
+}, {
+  id: 2,
+  title: "Todo 2",
+  descripton: "This is todo 2",
+  completed: false,
+}, {
+  id: 3,
+  title: "Todo 3",
+  descripton: "This is todo 3",
+  completed: false,
+
+}, {
+  id: 4,
+  title: "Todo 4",
+  descripton: "This is todo 4",
+  completed: false,
+}, {
+
+  id: 5,
+  title: "Todo 5",
+  descripton: "This is todo 5",
+  completed: false,
+}]
+
+app.get("/todos", (req, res) => {
+  const randomTodos = [];
+  for (let i = 0; i < 5; i++) {
+    if (Math.random() > 0.5) {
+      randomTodos.push(todos[i]);
+    }
+  }
+  res.json({
+    todos: randomTodos,
+  })
+});
 
 app.get("/sum", (req, res) => {
   const a = parseInt(req.query.a);

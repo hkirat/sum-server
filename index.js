8b1d41f04f8e781387dfb309e8fb96f867f768a0
Harkirat Singh <harkirat96@gmail.com>
2024-01-17 22:15:02 +0530

    Added things

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ app.use(cors());
 const todos = [{
   id: 1,
   title: "Todo 1",
-  descripton: "This is todo 1",
+  description: "This is todo 1",
   completed: false,
 }, {
   id: 2,
   title: "Todo 2",
-  descripton: "This is todo 2",
+  description: "This is todo 2",
   completed: false,
 }, {
   id: 3,
   title: "Todo 3",
-  descripton: "This is todo 3",
+  description: "This is todo 3",
   completed: false,
 
 }, {
   id: 4,
   title: "Todo 4",
-  descripton: "This is todo 4",
+  description: "This is todo 4",
   completed: false,
 }, {
 
   id: 5,
   title: "Todo 5",
-  descripton: "This is todo 5",
+  description: "This is todo 5",
   completed: false,
 }]
 

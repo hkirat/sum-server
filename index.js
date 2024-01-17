2001fcbe404c1dd64b6b5ad52829fbf810f5ceb3
Harkirat Singh <harkirat96@gmail.com>
2024-01-17 20:28:35 +0530

    added sum server

--- index.js
+++ index.js
@@ -64,4 +64,18 @@ app.get("/interest", (req, res) => {
 
 });
 
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
+app.get("/notifications", (req, res) => {
+  res.json({
+    network: getRandomNumber(10),
+    jobs: getRandomNumber(10),
+    messaging: getRandomNumber(10),
+    notifications: getRandomNumber(10)
+  })
+
+})
+
 app.listen(8080)
